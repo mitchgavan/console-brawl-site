1fa861698aa2847af6da80c1ae0bf6c46de7cd21
Mitch Gavan <mitchgavan@gmail.com>
2017-03-27 21:50:48 +1100

    calculated hero height

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,21 +5,39 @@ import imageMasterSystem from '../images/master-system.jpg';
 
 const HeroBlock = styled.div`
   display: flex;
+  overflow-y: hidden;
 `;
 
 const HeroItem = styled.div`
-
+  flex: 1 0 50%;
 `;
 
-const Hero = () => (
-  <HeroBlock>
-    <HeroItem>
-      <img src={imageGamecube} alt="Gamecube" />
-    </HeroItem>
-    <HeroItem>
-      <img src={imageMasterSystem} alt="Master System" />
-    </HeroItem>
-  </HeroBlock>
-);
+class Hero extends React.Component {
+
+  componentDidMount() {
+    const heroImage = document.getElementById('heroImage');
+    const heroElem = document.getElementById('hero');
+
+    heroImage.addEventListener('load', () => {
+      const carouselHeight = `${heroImage.clientHeight}px`;
+      heroElem.style.height = carouselHeight;
+    });
+  }
+
+  render() {
+    return (
+      <HeroBlock id="hero">
+        <HeroItem>
+          <img src={imageGamecube} id="heroImage" alt="Gamecube" />
+          <img src={imageGamecube} alt="Gamecube" />
+        </HeroItem>
+        <HeroItem>
+          <img src={imageMasterSystem} alt="Master System" />
+          <img src={imageGamecube} alt="Gamecube" />
+        </HeroItem>
+      </HeroBlock>
+    );
+  }
+}
 
 export default Hero;
