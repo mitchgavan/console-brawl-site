ee5026c7bb10f4ff307eccf4489dde71cda2decb
Mitch Gavan <mitchgavan@gmail.com>
2017-05-27 17:53:51 +1000

    feat: fetch games from API
    setup api and fetching data. still need to add limit/skip parameters to
    request.

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+  API_URL_RETRO: 'https://console-brawl-api-retro.now.sh/api',
+  API_URL_RECENT: 'https://console-brawl-api-recent.now.sh/api',
+  API_USERNAME: 'mitchg23',
+  API_PASSWORD: 'YKsh3ct6TY7Xe2Dn',
+  GIANT_BOMB_API_KEY: '2adf7337bc23815a927fb1276b938ad865f61832',
+};
+
+export default config;
--- pages/games.js
+++ pages/games.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { getRecentPlatformsGames } from '../store/games';
 import AppHead from '../components/app-head';
 import Header from '../components/header';
 import IntroGames from '../components/intro-games';
 import Search from '../components/search';
 import Footer from '../components/footer';
 
-export default () => (
-  <div>
-    <AppHead title="Games" />
-    <Header />
-    <IntroGames />
-    <Search />
-    <Footer />
-  </div>
-);
+export default class extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    getRecentPlatformsGames();
+  }
+
+  render() {
+    return (
+      <div>
+        <AppHead title="Games" />
+        <Header />
+        <IntroGames />
+        <Search />
+        <Footer />
+      </div>
+    );
+  }
+}
