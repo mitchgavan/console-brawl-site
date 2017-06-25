3f164398d47328c0dc950c8608e2e45ccd3bb4e1
Mitch Gavan <mitchgavan@gmail.com>
2017-06-25 21:40:20 +1000

    updated snapshot tests

--- components/__tests__/console-description.spec.js
+++ components/__tests__/console-description.spec.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import ConsoleDescription from '../console-list/console-description';
 
-test('<ConsoleDescription /> renders correctly', () => {
+test('It renders correctly', () => {
   const tree = shallow(
     <ConsoleDescription
       description="A cool console"
@@ -14,4 +14,15 @@ test('<ConsoleDescription /> renders correctly', () => {
     />,
   );
   expect(toJson(tree)).toMatchSnapshot();
+
+  const treeWithReverse = shallow(
+    <ConsoleDescription
+      description="A cool console"
+      developer="Nintendo"
+      name="Gamecube"
+      released="September 14, 2001"
+      reverseOrder
+    />,
+  );
+  expect(toJson(treeWithReverse)).toMatchSnapshot();
 });
--- components/__tests__/console-details.spec.js
+++ components/__tests__/console-details.spec.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import ConsoleDetails from '../console-list/console-details';
 
-test('<ConsoleDetails /> renders correctly', () => {
+test('It renders correctly', () => {
   const tree = shallow(
     <ConsoleDetails
       key="playstation-1"
@@ -12,9 +12,37 @@ test('<ConsoleDetails /> renders correctly', () => {
       developer="Sony"
       description="Very cool."
       slug="playstation-1"
-      reverseOrder
-      toggledOn="all"
+      reverseOrder={false}
+      toggledOn="All"
     />,
   );
   expect(toJson(tree)).toMatchSnapshot();
+
+  const treeReverseOrder = shallow(
+    <ConsoleDetails
+      key="playstation-1"
+      name="Playstation 1"
+      released="December 3, 1994"
+      developer="Sony"
+      description="Very cool."
+      slug="playstation-1"
+      reverseOrder
+      toggledOn="All"
+    />,
+  );
+  expect(toJson(treeReverseOrder)).toMatchSnapshot();
+
+  const treeToggledOff = shallow(
+    <ConsoleDetails
+      key="playstation-1"
+      name="Playstation 1"
+      released="December 3, 1994"
+      developer="Sony"
+      description="Very cool."
+      slug="playstation-1"
+      reverseOrder={false}
+      toggledOn="Nintendo"
+    />,
+  );
+  expect(toJson(treeToggledOff)).toMatchSnapshot();
 });
